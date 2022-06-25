7e76c5082c4974819163b275d4e319501f073618
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-25 22:16:04 +0300

    v4

--- app.py
+++ app.py
@@ -7,28 +7,10 @@ app = Flask(__name__)
 
 token = "5449738034:AAFSs_WqNNfPCUqFT_oo_v0JlCz8wAbh-iA"
 @app.route("/webhook", methods=['POST'])
-def webhook():
-    def start(update, context):
-            update.message.reply_text(a)
+def webhook(update, context):
+    a = telebot.TeleBot(token).send_message("-1001400179510", f"ticker = {ticker},price = {price},volume = {volume}")
+    update.message.reply_text(a)
 
-    updater = Updater(token)
-    updater.dispatcher.add_handler(CommandHandler("start",start))
-    updater.start_polling()
-    try:
-        data = json.loads(request.data)
-        ticker = data['ticker']
-        price = data['price']
-        volume = data['volume']
-        alert_tipe = data["alert_tipe"]
-        telegramUserId = data["telegramUserId"]
-
-        a = telebot.TeleBot(token).send_message(telegramUserId, f"ticker = {ticker},price = {price},volume = {volume}")
-
-   
-    except:
-        pass
-    return {
-        "code": "success",
-    }
-
-
+updater = Updater(token)
+updater.dispatcher.add_handler(CommandHandler("start",webhook))
+updater.start_polling()
