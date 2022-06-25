7da12eac96674ce2ce72bfea6411b6e6ea62dddd
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-25 22:18:44 +0300

    v5

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 token = "5449738034:AAFSs_WqNNfPCUqFT_oo_v0JlCz8wAbh-iA"
 @app.route("/webhook", methods=['POST'])
 def webhook(update, context):
-    a = telebot.TeleBot(token).send_message("-1001400179510", f"ticker = {ticker},price = {price},volume = {volume}")
+    a = telebot.TeleBot(token).send_message("-1001400179510", "ticker = {ticker},price = {price},volume = {volume}")
     update.message.reply_text(a)
 
 updater = Updater(token)
