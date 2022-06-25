b35e347dafc039724ed6568dddb01b8c00b08353
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-25 21:34:24 +0300

    v1

---
+++ app.py
@@ -0,0 +1,26 @@
+import telebot 
+from flask import Flask, request
+import json
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    try:
+        data = json.loads(request.data)
+        ticker = data['ticker']
+        price = data['price']
+        volume = data['volume']
+        alert_tipe = data["alert_tipe"]
+        telegramBotApi = data["telegramBotApi"]
+        telegramUserId = data["telegramUserId"]
+
+        telebot.TeleBot(telegramBotApi).send_message(telegramUserId, f"ticker = {ticker},price = {price},volume = {volume}")
+    except:
+        pass
+    return {
+        "code": "success",
+    }
