c1641e50be09f05a9fbd88fe34c65e1feb15a15a
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-25 22:23:41 +0300

    v2

--- app.py
+++ app.py
@@ -7,10 +7,18 @@ app = Flask(__name__)
 
 token = "5449738034:AAFSs_WqNNfPCUqFT_oo_v0JlCz8wAbh-iA"
 @app.route("/webhook", methods=['POST'])
-def webhook(update, context):
-    a = telebot.TeleBot(token).send_message("-1001400179510", "ticker = {ticker},price = {price},volume = {volume}")
-    update.message.reply_text(a)
+def webhook():
+    try:
+      def webhook(update, context):
+          a = telebot.TeleBot(token).send_message("-1001400179510", "ticker = {ticker},price = {price},volume = {volume}")
+          update.message.reply_text(a)
 
-updater = Updater(token)
-updater.dispatcher.add_handler(CommandHandler("start",webhook))
-updater.start_polling()
+      updater = Updater(token)
+      updater.dispatcher.add_handler(CommandHandler("start",webhook))
+      updater.start_polling()
+      
+    except:
+        pass
+    return {
+        "code": "success",
+    }
