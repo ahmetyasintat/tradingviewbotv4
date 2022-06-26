e953fcef488de3a60a84c8f58f2c0077a5a95fda
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-26 10:58:54 +0300

    v2

--- app.py
+++ app.py
@@ -14,13 +14,10 @@ def webhook():
             ticker = str(data['ticker'])
             price = str(data['price'])
             volume = str(data['volume'])
-            strategy_alert_message = str(data["strategy_alert_message"])
-            alert_type=str(data["alert_type"])
-            strategy_name = str(data["strategy_name"])
             telegramBotApi = str(data["telegramBotApi"])
             telegramUserId = str(data["telegramUserId"])
 
-            telebot.TeleBot(telegramBotApi).send_message(telegramUserId, f"ticker = {ticker}, price = {price},volume = {volume},strategy_alert_message = {strategy_alert_message},alert_type = {alert_type},strategy_name = {strategy_name}")
+            telebot.TeleBot(telegramBotApi).send_message(telegramUserId, f"ticker = {ticker}, price = {price},volume = {volume}")
    
         except:
             pass
