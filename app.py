538bad0b667039b40b71c839d1689956b099292e
ahmetyasintat <ahmet.yasin.ta@gmail.com>
2022-06-26 11:11:48 +0300

    v2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from distutils.log import debug
 import telebot 
-from flask import Flask, request, json
+from flask import Flask, request
 
 
 
@@ -14,14 +15,20 @@ def webhook():
             ticker = str(data['ticker'])
             price = str(data['price'])
             volume = str(data['volume'])
+            strategy_name = str(data["strategy_name"])
+            alert_type=str(data["alert_type"])
+            strategy_alert_message = str(data["strategy_alert_message"])
             telegramBotApi = str(data["telegramBotApi"])
             telegramUserId = str(data["telegramUserId"])
 
-            telebot.TeleBot(telegramBotApi).send_message(telegramUserId, f"ticker = {ticker}, price = {price},volume = {volume}")
-   
+            telebot.TeleBot(telegramBotApi).send_message(telegramUserId, f"ticker = {ticker}, price = {price},volume = {volume},strategy_name={strategy_name},alert_type = {alert_type},strategy_alert_message = {strategy_alert_message}")
+
         except:
             pass
          
         return {
             "code": "success",
         }
+    
+if __name__ == "__main__":
+    app.run(debug=True)
